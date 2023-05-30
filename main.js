4be27d3c11e4caa63c6b909e775f2e5a48557116
MarcosCampodonico <marcoscampodonico4550@outlook.com>
2023-05-30 13:56:19 -0300

    finalizado

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 let boton =document.getElementById("boton")
-let nombre=document.getElementById("username")
+let nombre=document.getElementById("cuadro")
 
 nombre.addEventListener("input", ()=>{
+    console.log(nombre.value)
     if(!nombre.value.includes("@")){
         nombre.className="colorfondo";
     }else{
