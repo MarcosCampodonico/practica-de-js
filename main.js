2fe42002a014e953a1a713dbf65976c84444278e
MarcosCampodonico <marcoscampodonico4550@outlook.com>
2023-06-03 19:13:28 -0300

    tercera pre entrega

--- main.js
+++ main.js
@@ -19,3 +19,15 @@ boton.addEventListener("mouseenter", ()=>{
 boton.addEventListener("mouseleave", ()=>{
     boton.style.background = "purple"
 })
+
+
+localStorage.setItem("nombre","marcos");
+localStorage.setItem("apellido","campodonico");
+let n1 = prompt("ingrese el producto")
+let n2 = prompt("ingrese el siguiente producto")
+const productos = [
+    {id: 1, producto: n1},
+    {id: 2, producto: n2},
+ ,
+]
+localStorage.setItem("carrito", JSON.stringify(productos));
