509df8f22aabeac718d8455d15f7dbfabf12cd61
MarcosCampodonico <marcoscampodonico4550@outlook.com>
2023-06-09 12:04:11 -0300

    tercera pre entrega

--- main.js
+++ main.js
@@ -1,34 +1,41 @@
 let boton =document.getElementById("boton")
 let nombre=document.getElementById("cuadro")
 
-nombre.addEventListener("input", ()=>{
-    console.log(nombre.value)
-    if(!nombre.value.includes("@")){
-        nombre.className="colorfondo";
-    }else{
-        nombre.className ="blanco";
-    }
+ nombre.addEventListener("input", ()=>{
+     if(!nombre.value.includes("@")){
+         nombre.className="colorfondo";
+     }else{
+         nombre.className ="blanco";
+     }
 });
 boton.addEventListener("click", ()=>{
     alert(`Bienvenido`)
+});
+
+ boton.addEventListener("mouseenter", ()=>{
+     boton.style.background = "red"
 })
+ boton.addEventListener("mouseleave", ()=>{
+     boton.style.background = "purple"
+});
 
-boton.addEventListener("mouseenter", ()=>{
-    boton.style.background = "red"
+
+
+let formulario = document.getElementById("form");
+const clientes = [];
+form.addEventListener('submit', function (event){
+    event.preventDefault();
+    const nombre = document.getElementById('cuadro').value;
+    const cliente = {};
+    console.log(nombre);
+
+    cliente['nombre']=nombre;
+    cliente.nombre=nombre;
+    clientes.push(cliente);
+    console.log(cliente);
+    console.log(clientes);
+
+    form.reset();
+
+
 })
-boton.addEventListener("mouseleave", ()=>{
-    boton.style.background = "purple"
-})
-
-
-localStorage.setItem("nombre","marcos");
-localStorage.setItem("apellido","campodonico");
-let n1 = prompt("ingrese el producto")
-let n2 = prompt("ingrese el siguiente producto")
-const productos = [
-    {id: 1, producto: n1},
-    {id: 2, producto: n2},
-]
-localStorage.setItem("carrito", JSON.stringify(productos));
-
-console.log(JSON.parse(productos))  
